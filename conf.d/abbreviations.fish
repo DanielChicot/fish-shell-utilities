7d3250d8a77b9a666ad6dbc96cff17a5656eec9f
Daniel Chicot <dan.chicot@gmail.com>
2025-09-12 09:30:00 +0100

    Add abbreviations for task development services

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -72,6 +72,8 @@ abbr -a -- r 'make run'
 abbr -a -- rcli 'docker exec -it concur-employee-listener-db-1 redis-cli --no-auth-warning -a password'
 abbr -a -- sis sort_in_situ
 abbr -a -- t task
+abbr -a -- tsu task development:services:up
+abbr -a -- tsd task development:services:down
 abbr -a -- tb task bootstrap
 abbr -a -- tf terraform
 abbr -a -- tfa terraform apply
